e42c616c9fefdbc3bb882043bfcb43b4aeba390d
Tyler Bailey <tbailey613@yahoo.com>
2020-12-07 17:03:12 -0800

    Completed CRUD endpoints

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const { getImages, deleteImages, insert } = require('./database/index.js');
 const database = require('./database/index.js');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/../client/dist')));
-// app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -24,7 +22,8 @@ app.get('/api/product/:id', (req, res) => {
   }, id);
 });
 
-app.delete('/api/product/', (req, res) => {
+app.delete('/api/product/:id', (req, res) => {
+  const { id } = req.params;
   database.deleteImages((err, response) => {
     if (err) {
       console.log('error deleting', err);
@@ -34,7 +33,7 @@ app.delete('/api/product/', (req, res) => {
       console.log('deleting data');
       res.status(200).send(response.rows);
     }
-  });
+  }, id);
 });
 
 app.post('/api/product/', async (req, res) => {
@@ -46,31 +45,20 @@ app.post('/api/product/', async (req, res) => {
   }
   console.log(response);
   res.status(200).send(response);
-  // database.insert((err, response) => {
-  //   if(err) {
-  //     console.log('error occurred');
-  //     console.log('response in error for post request is ', response);
-  //     res.status(404).send(err);
-  //   } else {
-  //     console.log('successful post response is ', response);
-  //     console.log('insert operation happened')
-  //     res.status(201).send(response.rows);
-  //   }
-  // });
 });
 
-app.put('/api/product', (req, res) => {
-  const { productName, images } = req.body;
-  const values = [productName, images];
-  const sql = 'UPDATE carousel SET ($1, $2)';
-  database.connection.client.query(sql, values, (err, response) => {
+app.put('/api/product/:id', (req, res) => {
+  const { id } = req.params;
+  database.update((err, response) => {
     if (err) {
-      res.status(500);
+      console.log('error with put request ', err);
+      res.status(404).send(err);
       res.end();
     } else {
-      res.status(200).send(response);
+      console.log('updating data ', response);
+      res.status(200).send(response.rows);
     }
-  });
+  }, id);
 });
 
 module.exports = app;
