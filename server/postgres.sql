7f059c173a4b6941371ec04c1cc0865e2eb1156c
Tyler Bailey <tbailey613@yahoo.com>
2020-12-07 19:32:21 -0800

    Refactored table schema

--- server/postgres.sql
+++ server/postgres.sql
@@ -4,23 +4,23 @@ CREATE DATABASE testdb;
 
 \c testdb
 
--- GRANT ALL PRIVILEGES ON DATABASE testdb TO postgres;
--- GRANT ALL ON ALL TABLES IN SCHEMA public TO postgres;
--- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO postgres;
+-- CREATE TABLE IF NOT EXISTS carousel (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   productName text
+-- );
 
 CREATE TABLE IF NOT EXISTS imagess (
   id SERIAL PRIMARY KEY NOT NULL,
   images text[]
+  -- productID int references carousel(id)
 );
 
-CREATE TABLE IF NOT EXISTS carousel (
-  id SERIAL PRIMARY KEY NOT NULL,
-  productName text
-);
+-- SELECT * FROM carousel CROSS JOIN imagess;
 
-\COPY carousel(productName) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/carousel.csv' DELIMITER ',' CSV HEADER;
+-- \COPY carousel(productName) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/carousel.csv' DELIMITER ',' CSV HEADER;
 
 \COPY imagess(images) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/images.csv' DELIMITER '$' CSV HEADER;
 
+-- SELECT * FROM carousel CROSS JOIN imagess;
 -- run this command:
 -- psql -U postgres -f server/postgres.sql
