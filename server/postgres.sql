d0a9acbdb29ee65bb4a972bad42413d3633b124a
Tyler Bailey <tbailey613@yahoo.com>
2020-12-08 17:38:25 -0800

    Created index on productName

--- server/postgres.sql
+++ server/postgres.sql
@@ -2,25 +2,23 @@ DROP DATABASE IF EXISTS testdb;
 
 CREATE DATABASE testdb;
 
-\c testdb
+\c testdb;
 
--- CREATE TABLE IF NOT EXISTS carousel (
---   id SERIAL PRIMARY KEY NOT NULL,
---   productName text
--- );
+CREATE TABLE IF NOT EXISTS carousel (
+  id SERIAL PRIMARY KEY NOT NULL,
+  productName text
+);
 
 CREATE TABLE IF NOT EXISTS imagess (
   id SERIAL PRIMARY KEY NOT NULL,
   images text[]
-  -- productID int references carousel(id)
 );
 
--- SELECT * FROM carousel CROSS JOIN imagess;
-
--- \COPY carousel(productName) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/carousel.csv' DELIMITER ',' CSV HEADER;
+\COPY carousel(productName) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/carousel.csv' DELIMITER ',' CSV HEADER;
 
 \COPY imagess(images) FROM '/Users/tylerbailey/Desktop/SDC/amazonCarousel/zains-service/server/csv/images.csv' DELIMITER '$' CSV HEADER;
 
--- SELECT * FROM carousel CROSS JOIN imagess;
+CREATE INDEX ON carousel(productName);
+
 -- run this command:
 -- psql -U postgres -f server/postgres.sql
